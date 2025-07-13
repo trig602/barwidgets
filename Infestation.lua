9fb9c12dfe061f84f640d86cd948c7d452f3ec72
trig602 <37033804+trig602@users.noreply.github.com>
2025-07-12 20:27:56 -0700

    Add files via upload

---
+++ Infestation.lua
@@ -0,0 +1,67 @@
+function widget:GetInfo()
+  return {
+    name      = "Infestation",
+    desc      = "Infestation",
+    author    = "trig602",
+    date      = "2025-07-07",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = true,
+  }
+end
+
+local myTeam = Spring.GetMyTeamID()
+local mapWidth = Game.mapX * 512
+local mapHeight = Game.mapY * 512
+local edgeGuardDistance = 200
+
+local function clamp(val, min, max)
+  if val < min then return min end
+  if val > max then return max end
+  return val
+end
+
+local function GetPointOnCircle(posX, posZ, range)
+    local angle = math.random() * 2 * math.pi
+    local offsetX = math.cos(angle) * range
+    local offsetZ = math.sin(angle) * range
+    local clampedX = clamp(posX + offsetX,edgeGuardDistance,mapWidth - edgeGuardDistance)
+    local clampedZ = clamp(posZ + offsetZ,edgeGuardDistance,mapHeight - edgeGuardDistance)
+    return clampedX, clampedZ
+end
+
+-- Supresses gaurd commands on newly created infestors
+function widget:UnitFinished(unitID, unitDefID, unitTeam)
+  if (unitDefID == 1389 and unitTeam == myTeam) then
+    local commands = Spring.GetUnitCommands(unitID, -1)
+      if commands and #commands > 0 then
+      local currentCmd = commands[1]
+        if currentCmd.id == CMD.GUARD then
+          Spring.GiveOrderToUnit(unitID, CMD.STOP, {}, {})
+      end
+    end
+  end 
+end
+
+--Orders idle infestors to either constructor a new infestor or fight to a nearby position.
+function widget:UnitIdle(unitID, unitDefID, unitTeam)
+  if (unitDefID == 1389 and unitTeam == myTeam) then
+
+    local randomChoice = math.random(1,2)
+
+    local unitPosX, unitPosZ
+    local unitOrderX, unitOrderZ
+
+    unitPosX, _ ,unitPosZ = Spring.GetUnitPosition(unitID)   
+    
+    if randomChoice == 1 then 
+      unitOrderX, unitOrderZ = GetPointOnCircle(unitPosX,unitPosZ,300)
+      Spring.GiveOrderToUnit(unitID,CMD.FIGHT,{unitOrderX,Spring.GetGroundHeight(unitOrderX,unitOrderZ),unitOrderZ},{})
+
+    elseif randomChoice == 2 then 
+      unitOrderX, unitOrderZ = GetPointOnCircle(unitPosX,unitPosZ,75)
+      Spring.GiveOrderToUnit(unitID,-1389,{unitOrderX,Spring.GetGroundHeight(unitOrderX,unitOrderZ),unitOrderZ},{})
+    end
+  end
+end
+
