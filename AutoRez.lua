e868a88502ad1647773a7f34f2f0aec8342d9bb3
trig602 <37033804+trig602@users.noreply.github.com>
2025-07-23 17:35:40 -0700

    Add files via upload

---
+++ AutoRez.lua
@@ -0,0 +1,58 @@
+function widget:GetInfo()
+  return {
+    name      = "AutoRez",
+    desc      = "AutoRez",
+    author    = "trig602",
+    date      = "2025-07-23",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = true,
+  }
+end
+
+local rezIDSet = {}
+local rezUnitList = {}
+local myTeamID = Spring.GetMyTeamID()
+local mapSize = math.max(Game.mapX * 512, Game.mapY * 512)
+
+for unitDefID, unitDef in pairs(UnitDefs) do
+    if unitDef.canResurrect then
+        rezIDSet[unitDefID] = true
+    end
+end
+
+function widget:Initialize()
+  rezUnitList = {}
+  for unitDefID, _ in pairs(rezIDSet) do    
+    local unitIDTable = Spring.GetTeamUnitsByDefs(myTeamID,unitDefID)
+    for _, unitID in ipairs(unitIDTable) do
+      rezUnitList[unitID] = true
+    end
+  end
+end
+
+function widget:MetaUnitAdded(unitID, unitDefID, unitTeam)
+  if rezIDSet[unitDefID] and unitTeam == myTeamID then    
+      rezUnitList[unitID] = true
+  end  
+end
+
+function widget:MetaUnitRemoved(unitID, unitDefID, unitTeam)
+    rezUnitList[unitID] = nil
+end
+
+function widget:GameFrame(frame)
+
+  if frame % 150 == 0 then
+    for unitID, _ in pairs(rezUnitList) do  
+
+      local commands = Spring.GetUnitCommands(unitID, -1)
+        if commands == nil or #commands == 0 then
+         local unitPosX, unitPosY, unitPosZ = Spring.GetUnitPosition(unitID)
+         Spring.GiveOrderToUnit(unitID,CMD.RESURRECT,{ unitPosX, unitPosY, unitPosZ, mapSize },{})
+      end
+
+    end
+  end
+end
+
